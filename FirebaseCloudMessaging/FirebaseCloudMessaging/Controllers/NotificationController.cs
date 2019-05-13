bb6848b35eae2f61f6c4446b8e34643898d6c207
bpenovic <bozo.penovic@lilcodelab.com>
2019-05-13 16:12:42 +0200

    Get token

---
+++ FirebaseCloudMessaging/FirebaseCloudMessaging/Controllers/NotificationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirebaseCloudMessaging.Controllers
+{
+    public class NotificationController : Controller
+    {
+        //[Authorize(Roles="Administrator")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FirebaseCloudMessaging/FirebaseCloudMessaging/wwwroot/js/init-firebase.js
@@ -0,0 +1,39 @@
+﻿// Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCA3jKj6fVgjExAelLodz_6JCzRu5janss",
+  authDomain: "dd2019-ccf43.firebaseapp.com",
+  databaseURL: "https://dd2019-ccf43.firebaseio.com",
+  projectId: "dd2019-ccf43",
+  storageBucket: "dd2019-ccf43.appspot.com",
+  messagingSenderId: "45562318462",
+  appId: "1:45562318462:web:b89ccd3744ec70a9"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve Firebase Messaging object.
+const messaging = firebase.messaging();
+
+messaging.requestPermission().then(function () {
+  console.log('Notification permission granted.');
+  messaging.getToken().then(function (currentToken) {
+    if (currentToken) {
+      console.log('Current token: ', currentToken);
+      //sendTokenToServer(currentToken);
+      //updateUIForPushEnabled(currentToken);
+    } else {
+      // Show permission request.
+      console.log('No Instance ID token available. Request permission to generate one.');
+      // Show permission UI.
+      //updateUIForPushPermissionRequired();
+      //setTokenSentToServer(false);
+    }
+  }).catch(function (err) {
+    console.log('An error occurred while retrieving token. ', err);
+    //showToken('Error retrieving Instance ID token. ', err);
+    //setTokenSentToServer(false);
+  });
+}).catch(function (err) {
+  console.log('Unable to get permission to notify.', err);
+});
